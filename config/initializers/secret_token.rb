7b273a9927fe0fc23fbda337f80faaef962f2d35
Aashish Saini <aashusaini2684@gmail.com>
2010-12-01 09:57:28 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sweetu::Application.config.secret_token = '3aa8eecb454fea57919aebd69ac79b53f6f1a82a70a5cb52c69b9d7ebd8badd2ae8ab39a6163527b6fad06bb2d01bd3e6fe49c28e9c4b49c2a419195bb6dedee'
